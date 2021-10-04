d806661e5dd610715cdb3d9ecfcdb9498420856e
chasblu <c.prit@ymail.com>
2021-10-04 13:39:54 -0400

    initial commit

---
+++ src/components/ShotOClock.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ShotOClock = () => {
+    return (
+        <div>
+            <button>Shot O'Clock</button>
+        </div>
+    );
+};
+
+export default ShotOClock;
